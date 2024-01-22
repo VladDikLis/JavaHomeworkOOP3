5b77d005ca3efedf7b9d104bc8e6d994d8b08011
VladLisitsyn <vladlisitsindiklis@yandex.ru>
2024-01-22 12:53:31 +0800

    homework

--- src/main/java/ru/otus/java/homework/Cat.java
+++ src/main/java/ru/otus/java/homework/Cat.java
@@ -5,5 +5,20 @@ public class Cat {
     Integer appetite;
     Boolean satiety;
 
+    public Cat(String name, Integer appetite) {
+        this.name = name;
+        this.appetite = appetite;
+        satiety = false;
+    }
+    public void eatCat() {
+        satiety = true;
+    }
+    public void appetiteCat() {
+        if (satiety) {
+            System.out.println(name + " наелся");
+        } else {
+            System.out.println(name + " голоден");
+        }
 
+    }
 }
